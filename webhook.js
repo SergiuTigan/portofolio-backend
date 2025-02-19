c4212cf59d94abcffb3ffd3c3b9f3154261343c7
root <root@srv716881.hstgr.cloud>
2025-02-19 17:27:13 +0000

    Add changes before restarting the server

--- webhook.js
+++ webhook.js
@@ -10,7 +10,7 @@ app.post("/webhook", (req, res) => {
   if (branch === "refs/heads/master") {
     console.log("🚀 Received push to master. Pulling latest code...");
 
-    exec("cd /home/ubuntu/blog-backend && git pull origin master && npm install && pm2 restart blog-api", (err, stdout, stderr) => {
+    exec("cd /home/ubuntu/blog-backend && git pull origin master && npm install && sudo systemctl restart blog-api", (err, stdout, stderr) => {
       if (err) {
         console.error(`❌ Error updating: ${err.message}`);
         return res.status(500).send("Update failed.");
