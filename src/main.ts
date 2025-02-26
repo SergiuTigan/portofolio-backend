db23024f4ef315fa0734d63896f9eb1323bf994f
root <root@srv716881.hstgr.cloud>
2025-02-26 04:35:31 +0000

    Enabled CORS

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,12 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors();
+  app.enableCors({
+    origin: 'http://localhost:4200', // Allow requests from Angular app
+    credentials: true, // Allow cookies/auth headers
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE', // Allow specific HTTP methods
+    allowedHeaders: 'Content-Type, Authorization', // Allow specific headers
+  });
   await app.listen(3000);
 }
 
