0bfb8009c1c205ff4131482ceccaba4ab5ba5642
SergiuTigan <sergiu@tigan.dev>
2025-03-01 16:33:30 +0200

    Test

--- src/app.module.ts
+++ src/app.module.ts
@@ -9,11 +9,11 @@ import { ConfigModule } from '@nestjs/config';
 
 @Module({
   imports: [
+    MongooseModule.forRoot('mongodb://localhost:27017/blog_db'),
     ConfigModule.forRoot({
       isGlobal: true, // Make config available throughout the app
       envFilePath: '.env', // Explicitly set the file path
     }),
-    MongooseModule.forRoot('mongodb://147.93.55.240:27017/blog_db'),
     JwtModule.register({
       secret: 'test123', // Use environment variables in production
       signOptions: { expiresIn: '24h' }, // Token expiration time
