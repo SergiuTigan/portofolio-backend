e96ae56637d501b375b71fe09b03d6968c0fbca5
SergiuTigan <sergiu@tigan.dev>
2025-03-11 13:48:09 +0200

    Added local Env

---
+++ .env.local.ts
@@ -0,0 +1,10 @@
+export const environment = {
+  BLOB_READ_WRITE_TOKEN: "vercel_blob_rw_wKiqHErAGLDqoX9r_KKFmAq6YdULjfUyRXC5zbHdSiG7SVS",
+  COMPANY_EMAIL: "sergiu@tigan.dev",
+  MAIL_FROM: "sergiu@tigan.dev",
+  MAIL_HOST: "mail.privateemail.com",
+  MAIL_PASSWORD: "marian93A@",
+  MAIL_PORT: "465",
+  MAIL_SECURE: "true",
+  MAIL_USER: "sergiu@tigan.dev"
+};
